0a5e005622d4d79f58ac56d10786264981feb543
aymen-git <aymen.mohammed.abdl@gmail.com>
2025-10-13 20:07:10 +0300

    finishing the new game

--- guessing.py
+++ guessing.py
@@ -2,9 +2,16 @@ import random
 
 roll = random.randint(1, 100)
 guess = int(input("Guess a Random Number Between 1 and 100"))
-
-while (guess < roll):
-  guess = int(inpu("Guess Higher!"))
-  
-
-
+guesses = 0
+while True:
+  if (roll > guess):
+    guesses += 1
+    print("guesses:", guesses)
+    guess = int(input("Guess Higher!"))
+  elif (roll < guess):
+    guesses += 1
+    print("guesses:", guesses)
+    guess = int(input("Guess Lower!"))
+  else:
+    print("You Won! it took you", guesses, "guesses!")
+    break

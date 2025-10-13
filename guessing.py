13d7f7294d1424e4bd67af51f9beea12f2a63e42
aymen-git <aymen.mohammed.abdl@gmail.com>
2025-10-13 22:16:54 +0300

    finished the lives system

--- guessing.py
+++ guessing.py
@@ -1,29 +1,36 @@
 import random
-dif = int(input("set difficulty: \n 1 = easy \n 2 = medium\n 3 = hard\n 4 extreme "))
-if (dif == 1):
-  guesses = 12
-elif (dif == 2):
-    guesses = 9
-elif (dif == 3):
-      guesses = 6
-elif (dif == 4):
+
+while True:
+    dif = int(input("set difficulty: \n 1 = easy \n 2 = medium\n 3 = hard\n 4 extreme "))
+    if (dif == 1):
+        guesses = 12
+        break
+    elif (dif == 2):
+        guesses = 9
+        break
+    elif (dif == 3):
+        guesses = 6
+        break
+    elif (dif == 4):
         guesses = 4
-else:
-          dif = int(input("Please Choose Numbers 1 through 4 to select difficultylty! "))
+        break
+    else:
+        print("Please Choose Numbers 1 through 4 to select difficulty!")
   
 roll = random.randint(1, 100)
 guess = int(input("Guess a Random Number Between 1 and 100"))
 
-while True:
-  if (roll > guess):
-    guesses -= 1
-    print("guesses left:", guesses)
-    guess = int(input("Guess Higher!"))
-  elif (roll < guess):
-    guesses += 1
-    print("guesses:", guesses)
-    guess = int(input("Guess Lower!"))
-  else:
-    print("You Won! You had", guesses, "guesses!")
-    
-    break
+while (guesses > 1):
+    if (roll > guess):
+        guesses -= 1
+        print("guesses left:", guesses)
+        guess = int(input("Guess Higher!"))
+    elif (roll < guess):
+        guesses -= 1
+        print("guesses:", guesses)
+        guess = int(input("Guess Lower!"))
+    else:
+        print("You Won! You had", guesses, "guesses!")
+        break
+
+print(f"GAME OVER! The Number Was {roll}")

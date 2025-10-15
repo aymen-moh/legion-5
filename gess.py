157809c462e11f3f4600301a052b64b4e59249a2
aymen-git <aymen.mohammed.abdl@gmail.com>
2025-10-15 18:11:26 +0300

    minor fixes

--- gess.py
+++ gess.py
@@ -1,20 +1,5 @@
 import random
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+import time
 
 
 
@@ -31,40 +16,52 @@ def roll():
     coc = input("PLEASE CHOOSE EITHER \"over\" OR \"under\": ")
   multiplier = 0 
   risk = 0
+  replay = ""
  
   
   while (money > 0):
-    dice = random.randint(1,100)
     money = money - bet
+    time.sleep(0.910) 
+    dice = random.randint(1,100)
+    print("Rollin'...")
+    print("ROLLED:", dice)
     if coc in ["over"]:
         risk = 100 - tar
     if coc in ["under"]:
         risk = tar - 1
     risk = risk / 100
-    multiplier = 0.98 / risk
-    print(multiplier)
+    multiplier = round(0.98 / risk, 2)
   
     if (coc == "over"):
-      if (dice > tar):
-        money = money * multiplier
+      if (tar < dice):
+        money = round(money + bet * multiplier,)
+        print(f"----YOU WON---- {money} With {multiplier}x Multiplier!")
+        print("")
         
+      else:
+        print(f"------YOU LOST------ you lost {bet} dollars and have {money} left")
+        print("")
+    if (coc == "under"):
+      if (tar > dice):
+        money = round(money + bet * multiplier,)
+        print(f"----YOU WON---- {money} With {multiplier}x Multiplier!")
+        print("")
         
-      
-      
-    elif coc == "under":
-      print(49)
-    
-    else:
-      ()
-      
-      
-roll()
+      else:
+        print(f"------YOU LOST------ you lost {bet} dollars and have {money} left")
+        print("")
+    if (money < 0):
+        print("YOU LOST ALL OF IT AND YOU ARE NOW IN DEBT!")
+    elif (money == 0):
+        print("YOU LOST ALL OF IT!")
+    if (money > 0):
+      replay = input("Press Anything To Play Again")
 
 
 def gg(): # GUESSING GAME FOR THOSE WHO ARE READING ALSO 67
-  
-  
-  lives = random.randint(5, 6)
+  multiplier = 24
+  money = 50000
+  lives = 5
   rn = random.randint(1, 100) 
   guess = int(input("Guess a Number Between 1 and 100!: "))
   if lives == 0:
@@ -72,8 +69,12 @@ def gg(): # GUESSING GAME FOR THOSE WHO ARE READING ALSO 67
     lives = -1
   while (lives > 0):
     if guess < rn:
+      multiplier = round(multiplier / 2,)
       print("Lives Left:", lives)
+      print(f"Current multiplier: {multiplier}")
       guess = int(input("Guess Higher!: "))
+      
+      
       lives -= 1
     elif guess > rn:
       print("Lives Left:", lives)
@@ -86,3 +87,4 @@ def gg(): # GUESSING GAME FOR THOSE WHO ARE READING ALSO 67
   if lives == 0:
     print("You Lost!")
     lives = -1
+gg()

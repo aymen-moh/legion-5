bca8e4c40cb7699f2706ab37415a92f52a36994f
aymen-git <aymen.mohammed.abdl@gmail.com>
2025-10-16 09:38:00 +0300

    started on new blackjack game

--- gess.py
+++ gess.py
@@ -2,15 +2,34 @@ import random
 import time
 
 def blackjack():
-  print("")
+  while True:
+    cards = (1, 2 , 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10)
+    rd = random.randint(0, 12) # random deck yall
+    fh = 0   # first hand   #
+    sh = 0   # second hand #
+    card_selection = cards[ud] # kool
+    hit = False
+    stand = False
+    while (ud < 21):
+
+
+blackjack()
+
+
+
+
+
+
+
+
+
 
 def roll():
   money = 50000
   bet = int(input("Enter The Amout of Money You Can lose: ")) 
   while (money < bet):
     print("You Only Have:", money)
-    bet = int(input(""))
-    
+    bet = int(input("Bet: ")) 
   tar = int(input("Choose a Target: "))
   coc = input("Choose Over or Under: ")
   while (coc != "over") and (coc != "under"):
@@ -18,9 +37,11 @@ def roll():
   multiplier = 0 
   risk = 0
   replay = ""
+  
  
   
   while (money > 0):
+    
     money = money - bet
     time.sleep(0.910) 
     dice = random.randint(1,100)
@@ -57,22 +78,24 @@ def roll():
         print("YOU LOST ALL OF IT!")
     if (money > 0):
       replay = input("Press Anything To Play Again")
-
-
 def gg(): # GUESSING GAME FOR THOSE WHO ARE READING ALSO 67
   while True:
     guess = 0
     money = 50000
     again = ""
     multiplier = 36.67
-    bet = int(input("Enter Your Bet!"))
+  
+    bet = int(input(""))
     lives = 6
     rn = random.randint(1, 100) 
     guess = int(input(f"Guess a Number Between 1 and 100!: {rn} "))
     win = False
-    
+    bet = int(input("Enter The Amout of Money You Can lose: ")) 
+  while (money < bet):
+    print("You Only Have:", money)
    
     while (lives > 0) and not win:
+      
     
       if guess < rn:
         multiplier = round(multiplier / 2, 2)
@@ -105,6 +128,3 @@ def gg(): # GUESSING GAME FOR THOSE WHO ARE READING ALSO 67
       print("You Are Broke!")
     again = input("Press Anything To Play Again!")
     win = False
-  
-  
-gg()

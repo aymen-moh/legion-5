d721a61b00488b9203c2c5e6018baac96395b08f
aymen-git <aymen.mohammed.abdl@gmail.com>
2025-11-01 15:32:27 +0300

    started learning and playing with turtle module to make a snake game.

---
+++ cube.py
@@ -0,0 +1,14 @@
+import turtle
+window = turtle.Screen()
+window.title("snake")
+window.bgcolor("green")
+window.setup(Width=600, height= 600)
+window.tracer(0)
+Shead = turtle.turtle
+Shead.speed(0)
+
+
+
+
+
+window.mainloop
